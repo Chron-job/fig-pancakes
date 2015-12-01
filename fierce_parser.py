eba83665dc788224315e544e3e2c2811c092f753
Chron-job <crnewt26@gmail.com>
2015-12-01 13:44:16 -0500

    Updated fierce script to verify input and output file names

--- fierce_parser.py
+++ fierce_parser.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
+# 
+# Chris Newton 
+# V 1.1
 # Take input from fierce DNS scan and ouput excel file
 # Usage fierce_parser.py -i <input filename> -o <output filename>
+# If no output file is specified, the input filename will be used by default
 
-import sys, getopt, xlwt
+import sys, getopt, xlwt, os
 
 def main(argv):
-#Variables	
+# Variables	
 	inputfile = ' '
 	outputfile = ' '
 	book = xlwt.Workbook()
 	sh = book.add_sheet("Sheet1")
 	n = 0
 	
+# Verify input parameters
+	if len(sys.argv) < 2:
+		print 'fierce_parser.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
 	try:
 		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
 	except getopt.GetoptError:
@@ -25,17 +33,30 @@ def main(argv):
 			inputfile = arg
 		elif opt in ("-o", "--ofile"):
 			outputfile = arg
+	if outputfile == ' ':
+		outputfile = inputfile + ".xlsx"
+
+# Read input file into list		
 	file = open(inputfile, 'r')
 	lines = file.readlines()
 	file.close()
+
+# Write list to file	
 	for line in lines:
 		if line[0].isdigit():
 			domain = line.split( )
 			sh.write(n,0,domain[0])
 			sh.write(n,1,domain[1])
 			n = n+1
-	book.save(outputfile)
-	print "File output: " + outputfile
+
+# Save file and print confirmation			
+	if outputfile.endswith(".xlsx"):
+		book.save(outputfile)
+		print "New file: " + os.getcwd() + "/" + outputfile
+	else:
+		book.save(outputfile + ".xlsx")
+		print "New file: " + os.getcwd() + "/" + outputfile + ".xlsx"
+	
 if __name__ == "__main__":
 	main(sys.argv[1:])
 	
